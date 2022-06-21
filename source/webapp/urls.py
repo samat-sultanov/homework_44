8b067d5fb76e500b6a50f3f0b86de1df31fb0acb
SamatSultanov <samatsayak@gmail.com>
2022-06-21 18:48:49 +0600

    create stats.html, modified stats_page function

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from webapp.views import game_page
+from webapp.views import game_page, stats_page
 
 
 urlpatterns = [
-    path('', game_page)
+    path('', game_page),
+    path('stats/', stats_page)
 ]
